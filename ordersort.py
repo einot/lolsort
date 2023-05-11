420ddcba293757a348dc6b486aed016729f066f3
Eino Tuominen <eino@utu.fi>
2023-05-11 08:21:53 +0300

    rename simplesort to ordersort

--- ordersort.py
+++ ordersort.py
@@ -1,4 +1,4 @@
-# lolsort, et@iki.fi and java@iki.fi
+# ordersort, et@iki.fi and java@iki.fi
 # inspired by https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.116.9158&rep=rep1&type=pdf
 
 import itertools
@@ -6,16 +6,15 @@ import itertools
 def orderliness(sequence):
     """calculate the orderliness of the sequence
 
-    orderliness is a count of all ordered pairs in the sequence
+    orderliness is the count of all ordered pairs in the sequence
     """
     orderliness = 0
     for a, b in zip(sequence, sequence[1:]):
         orderliness += 1 if a <= b else 0
     return orderliness
         
-def maximum(sequence):
-    """ find the element with maximally ordered element from 
-    the sequence using recursion """
+def most_ordered(sequence):
+    """ find the maximally ordered element from the sequence"""
     def findmax(a, n):  
         if (n == 1):
             return a[0]
@@ -23,13 +22,15 @@ def maximum(sequence):
 
     return findmax(sequence, len(sequence))
 
-def lolsort(sequence):
-    """ Lolsort, an incredibly inefficient sorting algorithm
+def simplesort(sequence):
+    """ ordersort, an incredibly inefficient sorting algorithm
 
     Sort the sequence by first multiplying the problem by 
     creating all possible permutations of the sequence
     and then finding the most ordered one.
+
+    Time complexity O(e^(n!))
     """
     permutations = list(itertools.permutations(sequence))
     # return the most orderly permutation
-    return maximum(permutations)
+    return most_ordered(permutations)
